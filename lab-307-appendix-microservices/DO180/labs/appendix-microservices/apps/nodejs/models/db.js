005e16f8edf8b8b80d9b1e5324a2124b60fd3230
Student User <student@workstation.lab.example.com>
2023-04-02 18:21:38 -0400

    lab-307-appendix-microservices

---
+++ lab-307-appendix-microservices/DO180/labs/appendix-microservices/apps/nodejs/models/db.js
@@ -0,0 +1,12 @@
+
+module.exports.params = {
+    dbname: "items",
+    username: "user1",
+    password: "mypa55",
+    params: {
+        host: "mysql",
+        port: "3306",
+        dialect: 'mysql'
+    }
+};
+
